b4353e526c90664734324feb36a6bf77bdcaeeda
Daniel Wojda <wojdadan@gmail.com>
2015-09-23 16:55:15 +0200

    Implement Rest api using "traditional" controller instead of Spring-Data-Rest. Reason: Spring-Data-Rest is using Hateoas. For hackathon purpose I want to use Faign on rest api side. Unfortunately I couldn't configure Faign to parse Hateoas.

--- src/main/java/org/bmshackathon/VideoMetadataController.java
+++ src/main/java/org/bmshackathon/VideoMetadataController.java
@@ -10,27 +10,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 public class VideoMetadataController {
+
     private  VideoMetadataRepository videoMetadataRepository;
 
     @Autowired
     public VideoMetadataController(VideoMetadataRepository videoMetadataRepository) {
         this.videoMetadataRepository = videoMetadataRepository;
     }
-//
-//    @RequestMapping(value = "/videos/{videoUuid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<VideoMetadata> findOne(@PathVariable Long videoUuid) {
-//        return videoMetadataRepository.findOne(videoUuid)
-//                .map(videoMetadata -> new ResponseEntity<>(videoMetadata, HttpStatus.OK))
-//                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
-//    }
 
-//    nie umiem :<
-//    public HttpEntity<VideoMetadata> findAll() {
-//        Iterable<VideoMetadata> all = videoMetadataRepository.findAll();
-//    }
+    @RequestMapping(value = "/videos/{videoUuid}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<VideoMetadata> findOne(@PathVariable Long videoUuid) {
+        return videoMetadataRepository.findOne(videoUuid)
+                .map(videoMetadata -> new ResponseEntity<>(videoMetadata, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @RequestMapping(value = "/videoMetadatas", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public HttpEntity<Iterable<VideoMetadata>> findAll() {
+        return new ResponseEntity<>(videoMetadataRepository.findAll(), OK);
+    }
 
 }
--- src/main/java/org/bmshackathon/VideoMetadataRepository.java
+++ src/main/java/org/bmshackathon/VideoMetadataRepository.java
@@ -1,13 +1,13 @@
 package org.bmshackathon;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.Repository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
 
-@RepositoryRestResource
-public interface VideoMetadataRepository extends CrudRepository<VideoMetadata, Long> {
+//@RepositoryRestResource
+public interface VideoMetadataRepository extends Repository<VideoMetadata, Long> {
 
-//    Optional<VideoMetadata> findOne(Long uuid);
+    Optional<VideoMetadata> findOne(Long uuid);
+
+    Iterable<VideoMetadata> findAll();
 }
