bfb94ce69efaf9d712a75788f0eb2673cb8057ce
p-bms <p-bms@users.noreply.github.com>
2015-09-26 14:01:01 +0200

    Added Spring Cloud Sleuth

--- src/main/java/org/bmshackathon/MetadataStoreApplication.java
+++ src/main/java/org/bmshackathon/MetadataStoreApplication.java
@@ -3,10 +3,18 @@ package org.bmshackathon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class MetadataStoreApplication {
+
+    @Bean
+    public Sampler<?> defaultSampler() {
+        return new AlwaysSampler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MetadataStoreApplication.class, args);
